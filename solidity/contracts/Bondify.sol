fd1aef5996aba3ab549b94da5b320392cbf1f52b
Jwalin Joshi <jwalinjoshi@MacBook-Pro.local>
2021-09-22 15:28:50 -0700

    working on scheduling bond payouts

--- solidity/contracts/Bondify.sol
+++ solidity/contracts/Bondify.sol
@@ -3,36 +3,53 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 
-contract Bondify is ERC721URIStorage {
+// interface Aion
+contract Aion {
+    uint256 public serviceFee;
+    function ScheduleCall(uint256 blocknumber, address to, uint256 value, uint256 gaslimit, uint256 gasprice, bytes memory data, bool schedType) public payable returns (uint,address);
+
+}
+
+contract Bondify is ERC721URIStorage, ERC721Burnable {
 
   uint256 public tokenCounter;
   enum Compounding{ANNUAL, MONTHLY, WEEKLY, DAILY, CONTINOUS}
   mapping(uint256 => address) public itemIdToSender;
   mapping(uint256 => string) public itemIdToTokenURI;
-  mapping (uint256 => Compounding) public uintToCompounding;
-  mapping (uint256 => Compounding) tokenIdToCompounding;
-  mapping (uint256 => uint256) tokenIdToInterestRate;
+  mapping(uint256 => uint256) public tokenIdToPrincipal;
+  mapping(uint256=> uint256) public tokenIdToPayout; 
   mapping (uint256 => uint256) tokenIdToIssuanceDate;
   mapping (uint256 => uint256) tokenIdToExpiryDate;
+  Aion aion;
 
   constructor() public ERC721("Bond", "BOND") {
     tokenCounter = 0;
     }
-  function createBond(string memory bondURI,uint256 compoundInt, uint256 interestRate, uint256 expiryDate) public returns (uint256) {
+
+
+  function verifyPayout(string memory bondURI) {
+    itemID = 
+  }
+
+
+  function createBond(string memory bondURI,uint256 principal,uint256 payout,uint256 expiryDate) public returns (uint256) {
     uint256 newItemId = tokenCounter;
     _safeMint(msg.sender, newItemId);
     itemIdToSender[newItemId] = msg.sender;
     _setTokenURI(newItemId, bondURI);
     itemIdToTokenURI[newItemId] = bondURI;
     tokenCounter = tokenCounter + 1;
-    Compounding compounding = Compounding(compoundInt);
-    tokenIdToCompounding[newItemId] = compounding;
-    tokenIdToInterestRate[newItemId] = interestRate;
+    tokenIdToPrincipal[newItemId] = principal;
+    tokenIdToPayout[newItemId] = payout;
     tokenIdToIssuanceDate[newItemId] = block.timestamp;
     tokenIdToExpiryDate[newItemId] = expiryDate;
     return newItemId;
 
   }
+
+
+
+  //TODO: figure out how to do bond payouts
 
 
   //TODO: figure out how to access structs/display mapping info
