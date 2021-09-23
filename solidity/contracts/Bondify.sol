604f0ca48ab80c63b03752a432f8ab859f651119
Jwalin Joshi <jwalinjoshi@MacBook-Pro.local>
2021-09-22 17:24:01 -0700

    added proper transferring of funds

--- solidity/contracts/Bondify.sol
+++ solidity/contracts/Bondify.sol
@@ -5,7 +5,8 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 
 contract Bondify is ERC721URIStorage {
-
+  event Deposit(address sender, uint amount);
+  event Withdrawal(address receiver, uint amount);
   uint256 public tokenCounter;
   enum Compounding{ANNUAL, MONTHLY, WEEKLY, DAILY, CONTINOUS}
   mapping(uint256 => address) public itemIdToSender;
@@ -14,27 +15,16 @@ contract Bondify is ERC721URIStorage {
   mapping(uint256=> uint256) public tokenIdToPayout; 
   mapping (uint256 => uint256) tokenIdToIssuanceDate;
   mapping (uint256 => uint256) tokenIdToExpiryDate;
+  mapping (address => uint256) ETHBalances;
 
   constructor() public ERC721("Bond", "BOND") {
     tokenCounter = 0;
     }
 
 
-  function excersizeBond(uint256 tokenId) external {
-    require(_exists(tokenId), "ERC721URIStorage: URI query for nonexistent token");
-    uint256 expiry = tokenIdToExpiryDate[tokenId];
-    uint256 payout = tokenIdToPayout[tokenId];
-    address issuer = itemIdToSender[tokenId];
-    address owner = ownerOf(tokenId);
-    require(owner == msg.sender, "you do not own this bond");
-    require(owner != issuer, "you cannot excersize a bond you issued");
-    require(expiry <= block.timestamp, "tried to get bond payout before bond expires!");
-    //send something to a payments contract
-    emit Transfer(issuer, owner, payout);
-
-  }
 
 
+  //called by bond issuer to create bond
   function createBond(uint256 principal,uint256 payout,uint256 expiryDate) external returns (uint256) {
     verifyBond(principal, payout, expiryDate);
     uint256 newItemId = tokenCounter;
@@ -58,11 +48,40 @@ contract Bondify is ERC721URIStorage {
     require(expiryDate > block.timestamp, 'bond must expire in the future');
   }
 
+    //called by owner of bond to get payment after expiry
+  function excersizeBond(uint256 tokenId) external payable {
+    require(_exists(tokenId), "ERC721URIStorage: URI query for nonexistent token");
+    uint256 expiry = tokenIdToExpiryDate[tokenId];
+    uint256 payout = tokenIdToPayout[tokenId];
+    address issuer = itemIdToSender[tokenId];
+    address owner = ownerOf(tokenId);
+    require(owner == msg.sender, "you do not own this bond");
+    require(owner != issuer, "you cannot excersize a bond you issued");
+    require(expiry <= block.timestamp, "tried to get bond payout before bond expires!");
+    //send something to a payments contract
+    require(ETHBalances[issuer]>= payout, 'not enough funds deposited by issuer to honor bond!');
+    //call default function
+    ETHBalances[issuer] = ETHBalances[issuer] - (payout);
+    ETHBalances[owner] = ETHBalances[owner] + (payout);
+    emit Transfer(issuer, owner, payout);
+    //todo use https://consensys.net/diligence/blog/2019/09/stop-using-soliditys-transfer-now/ instead of transfer
+
+  }
+
+  function depositETH() external payable {
+    emit Deposit(msg.sender, msg.value);
+    ETHBalances[msg.sender] += msg.value;
+
+  }
+
+  function withdrawETH() external payable {
+    emit Withdrawal(msg.sender, msg.value);
+    ETHBalances[msg.sender] -= msg.value;
+
+  }
 
 
-  //TODO: figure out how to do bond payouts
-
-  //TODO: figure out how to access structs/display mapping info
+  //TODO: figure out collateral (use NFTs as collateral?)
 
   //TODO: create deployment scripts from here (https://betterprogramming.pub/how-to-create-nfts-with-solidity-4fa1398eb70a)
 
